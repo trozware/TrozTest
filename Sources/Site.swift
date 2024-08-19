eaaf9425d02ac54df6df278c983cd4c03cad0945
Sarah Reichelt <sarah@troz.net>
2024-08-19 11:20:48 +1000

    first commit

---
+++ Sources/Site.swift
@@ -0,0 +1,27 @@
+import Foundation
+import Ignite
+
+@main
+struct IgniteWebsite {
+    static func main() async {
+        let site = ExampleSite()
+
+        do {
+            try await site.publish()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
+
+struct ExampleSite: Site {    
+    var name = "Troz"
+    var titleSuffix = " – Test"
+    var url = URL(string: "https://www.example.com")!
+    var builtInIconsEnabled = true
+
+    var author = "Sarah Reichelt"
+
+    var homePage = Home()
+    var theme = MyTheme()
+}
