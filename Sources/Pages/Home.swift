036abda432fd244e09821759933963c1fab8885b
Sarah Reichelt <sarah@troz.net>
2024-08-19 11:40:35 +1000

    edit home page

--- Sources/Pages/Home.swift
+++ Sources/Pages/Home.swift
@@ -5,7 +5,9 @@ struct Home: StaticPage {
     var title = "Home"
 
     func body(context: PublishingContext) -> [BlockElement] {
-        Text("Testing")
+        Text("Page One")
             .font(.title1)
+
+        Text("This is a test site so I can check out the Ignite framework and how to serve it.")
     }
 }
